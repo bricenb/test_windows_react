d4b08f75f19607345605c349abdf7ec251df585e
bricen <bricen@email.sc.edu>
2021-10-20 14:17:28 -0400

    added login and CSS

---
+++ src/component/login.jsx
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
+
+
+class Login extends React.Component {
+
+    render () {
+        return (
+            <html>
+                <header>
+                    <div id="head">
+                    <h1 id="headp">Link2RSVP</h1>
+                    </div>
+                    </header>
+            <div id="signin">
+                <Form>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Please Enter an Email address</Form.Label>
+                        <Form.Control id="email" type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                             We'll never share your email with anyone else.
+                        </Form.Text>
+                    </Form.Group>
+
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Please Enter your Password</Form.Label>
+                        <Form.Control id="pass" type="password" placeholder="Password" />
+                    </Form.Group>
+                   
+                    <Button id="signinbutton" variant="primary" type="submit">
+                        Sign In
+                    </Button>
+                </Form>
+            </div>
+            </html>
+        );
+    }
+ }
+
+export default Login;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Login from './component/login';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
   document.getElementById('root')
 );
